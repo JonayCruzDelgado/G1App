596b583c2219a31a591a7c36d6deda50e32698a9
marta <marta.jimenez@alu.ulpgc.es>
2017-03-14 13:00:17 +0000

    crear RealmObjects

---
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/dataBase/Autor.java
@@ -0,0 +1,61 @@
+package es.ulpgc.eite.clean.mvp.sample.dataBase;
+
+import android.graphics.Bitmap;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by Marta on 14/03/2017.
+ */
+
+public class Autor extends RealmObject {
+
+    @PrimaryKey
+    private String id;
+    private String nombre;
+    private String descripcion;
+    private Bitmap imagen;
+    private RealmList<Obra> obras;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Bitmap getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(Bitmap imagen) {
+        this.imagen = imagen;
+    }
+
+    public RealmList<Obra> getObras() {
+        return obras;
+    }
+
+    public void setObras(RealmList<Obra> obras) {
+        this.obras = obras;
+    }
+}
---
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/dataBase/Categoria.java
@@ -0,0 +1,42 @@
+package es.ulpgc.eite.clean.mvp.sample.dataBase;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by Marta on 14/03/2017.
+ */
+
+public class Categoria extends RealmObject {
+
+    @PrimaryKey
+    private String id;
+    private String categoria;
+    private RealmList<Autor> autores;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public RealmList<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(RealmList<Autor> autores) {
+        this.autores = autores;
+    }
+}
