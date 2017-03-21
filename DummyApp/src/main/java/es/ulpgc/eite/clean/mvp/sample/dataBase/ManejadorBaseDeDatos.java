5781e2320c85c6bdd278eeb5afdcda2e1390ad70
marta <marta.jimenez@alu.ulpgc.es>
2017-03-21 11:04:58 +0000

    empezar a añadir algo a la base de datos, sin probar aun

--- DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/dataBase/ManejadorBaseDeDatos.java
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/dataBase/ManejadorBaseDeDatos.java
@@ -34,8 +34,8 @@ class ManejadorBaseDeDatos {
         return result.get(0).getCategoria();
     }
 
-    public String[] getListaAutores(int idCategoria){
-        RealmResults<Autor> result =realm.where(Autor.class).equalTo("id",idCategoria).findAll(); //encuentra los autores de esa categoria
+    public String[] getListaAutores(String nombreCategoria){
+        RealmResults<Autor> result =realm.where(Autor.class).equalTo("categoria",nombreCategoria).findAll(); //encuentra los autores de esa categoria
 
         String[] array={""};
         int i;
@@ -44,8 +44,8 @@ class ManejadorBaseDeDatos {
         }
         return array;
     }
-    public String[] getListaObras(int idAutor){
-        RealmResults<Obra> result =realm.where(Obra.class).equalTo("id",idAutor).findAll();
+    public String[] getListaObras(int nombreAutor){
+        RealmResults<Obra> result =realm.where(Obra.class).equalTo("nombre",nombreAutor).findAll();
 
         String[] array={""};
         int i;
@@ -89,15 +89,15 @@ class ManejadorBaseDeDatos {
     }
 
     public void initBaseDeDatos(){
-        realm.beginTransaction();
+
         // toca rellener a saco aqui
         addCategoria("Escultura");
         addCategoria("Arquitectura");
         addCategoria("Pintura");
 
-        String nombreAutor1 ="Miguel Ángel";
-        String categoriaAutor1 ="Escultura";
-        String descripcionAutor1 ="Michelangelo Buonarroti (Caprese, 6 de marzo de 1475-Roma, " +
+        String nombreAutor ="Miguel Ángel";
+        String categoriaAutor ="Escultura";
+        String descripcionAutor ="Michelangelo Buonarroti (Caprese, 6 de marzo de 1475-Roma, " +
                 "18 de febrero de 1564), conocido en español como Miguel Ángel, fue un arquitecto, " +
                 "escultor y pintor italiano renacentista, considerado uno de los más grandes artistas de la historia tanto por " +
                 "sus esculturas como por sus pinturas y obra arquitectónica. Desarrolló su labor artística a lo largo de más de " +
@@ -105,12 +105,19 @@ class ManejadorBaseDeDatos {
                 "los diferentes papas romanos.";
         String fileName = "/miguel_anguel.jpg";
         File file1 = new File(fileName);
-        Bitmap imagenAutor1 = BitmapFactory.decodeFile(file1.getAbsolutePath());
+        Bitmap imagenAutor = BitmapFactory.decodeFile(file1.getAbsolutePath());
 
-        addAutor(nombreAutor1,descripcionAutor1,categoriaAutor1,imagenAutor1);
+        addAutor(nombreAutor,descripcionAutor,categoriaAutor,imagenAutor);
 
+        nombreAutor ="";
+        categoriaAutor="";
+        descripcionAutor="";
+        fileName = "/miguel_anguel.jpg";
+        File file2 = new File(fileName);
+        imagenAutor = BitmapFactory.decodeFile(file2.getAbsolutePath());
 
-        realm.commitTransaction();
+        addAutor(nombreAutor,descripcionAutor,categoriaAutor,imagenAutor);
+
     }
 // crear categoria, no se añaden autores por que voy a hacer que un autor solo pueda pertenecer a una categoria
 // por lo que se especifica al crear el autor, es mucho mas simple asi, si lo quieren hacer que un autor pertenezca a varias categoria es mas lio
