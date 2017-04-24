248b85e6b8cd1e4a38fe23ab38539df56777c4a8
marta <marta.jimenez@alu.ulpgc.es>
2017-04-24 18:24:14 +0100

    test comprobar lista autores

--- DummyApp/src/androidTest/java/es/ulpgc/eite/clean/mvp/sample/inicial/RealmTestInicial.java
+++ DummyApp/src/androidTest/java/es/ulpgc/eite/clean/mvp/sample/inicial/RealmTestInicial.java
@@ -11,15 +11,19 @@ import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.anything;
 import static org.junit.Assert.assertEquals;
 
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.TextView;
 
 import org.junit.Rule;
 
 import es.ulpgc.eite.clean.mvp.sample.R;
+import es.ulpgc.eite.clean.mvp.sample.dataBase.ManejadorBaseDeDatos;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -44,10 +48,32 @@ public class RealmTestInicial {
       onView(allOf(withId(R.id.textoBtnEscultura), withText("Escultura")));
   }
     @Test
-    public void testCargarlistaAutores() {
+    public void testDisplaylistaAutoresEscultura() {
         onView(withId(R.id.btnEscultura)).perform(click());
         onView(allOf(withId(R.id.listaAutores),isDisplayed()));
-
+        onData(anything())
+                .inAdapterView(withId(R.id.listaAutores))
+                .atPosition(0)
+                .check(matches(withText("Miguel Ángel")));
+    }
+    @Test
+    public void testDisplaylistaAutoresArquitectura() {
+        onView(withId(R.id.btnArquitectura)).perform(click());
+        onView(allOf(withId(R.id.listaAutores),isDisplayed()));
+        onData(anything())
+                .inAdapterView(withId(R.id.listaAutores))
+                .atPosition(0)
+                .check(matches(withText("Le Corbusier")));
+    }
+    @Test
+    public void testDisplaylistaAutoresPintura() {
+        onView(withId(R.id.btnPintura)).perform(click());
+        onView(allOf(withId(R.id.listaAutores),isDisplayed()));
+        onData(anything())
+                .inAdapterView(withId(R.id.listaAutores))
+                .atPosition(0)
+                .check(matches(withText("Leonardo Da Vinci")));
 
     }
+
 }
