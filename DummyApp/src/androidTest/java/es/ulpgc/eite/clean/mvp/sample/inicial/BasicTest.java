2d60cd447870d9a0f3f081524f1f17d1f0373877
marta <marta.jimenez@alu.ulpgc.es>
2017-04-25 08:56:44 +0100

    arreglo en BasicTest

--- DummyApp/src/androidTest/java/es/ulpgc/eite/clean/mvp/sample/inicial/BasicTest.java
+++ DummyApp/src/androidTest/java/es/ulpgc/eite/clean/mvp/sample/inicial/BasicTest.java
@@ -7,17 +7,22 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import org.junit.Rule;
@@ -39,17 +44,19 @@ public class BasicTest {
   @Test
   public void testBtnsIncialDisplay() {
       onView(allOf(withId(R.id.btnPintura), isDisplayed()));
-      onView(allOf(withId(R.id.textoBtnPintura), withText("Pintura")));
+      onView(withId(R.id.textoBtnPintura)).check(matches(withText("Pintura")));
 
       onView(allOf(withId(R.id.btnArquitectura), isDisplayed()));
-      onView(allOf(withId(R.id.textoBtnArquitectura), withText("Arquitectura")));
+      onView(withId(R.id.textoBtnArquitectura)).check(matches(withText("Arquitectura")));
 
       onView(allOf(withId(R.id.btnEscultura), isDisplayed()));
-      onView(allOf(withId(R.id.textoBtnEscultura), withText("Escultura")));
+      onView(withId(R.id.textoBtnEscultura)).check(matches(withText("Escultura")));
   }
     @Test
     public void testDisplaylistaAutoresEscultura() {
         onView(withId(R.id.btnEscultura)).perform(click());
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class)))) //comprobar el titulo de Toolbar
+                .check(matches(withText("Escultura")));
         onView(allOf(withId(R.id.listaAutores),isDisplayed()));
         onData(anything())
                 .inAdapterView(withId(R.id.listaAutores))
@@ -59,6 +66,8 @@ public class BasicTest {
     @Test
     public void testDisplaylistaAutoresArquitectura() {
         onView(withId(R.id.btnArquitectura)).perform(click());
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Arquitectura")));
         onView(allOf(withId(R.id.listaAutores),isDisplayed()));
         onData(anything())
                 .inAdapterView(withId(R.id.listaAutores))
@@ -68,6 +77,8 @@ public class BasicTest {
     @Test
     public void testDisplaylistaAutoresPintura() {
         onView(withId(R.id.btnPintura)).perform(click());
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Pintura")));
         onView(allOf(withId(R.id.listaAutores),isDisplayed()));
         onData(anything())
                 .inAdapterView(withId(R.id.listaAutores))
@@ -81,7 +92,8 @@ public class BasicTest {
                 .inAdapterView(withId(R.id.listaAutores))
                 .atPosition(0)
                 .perform(click());
-        onView(allOf(withId(R.id.toolbar),withText("Miguel Ángel")));
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Miguel Ángel")));
         onView(allOf(withId(R.id.listaObras),isDisplayed()));
         onData(anything())
                 .inAdapterView(withId(R.id.listaObras))
@@ -99,13 +111,15 @@ public class BasicTest {
                 .inAdapterView(withId(R.id.listaObras))
                 .atPosition(0)
                 .perform(click());
-        onView(allOf(withId(R.id.toolbar),withText("El David")));
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("El David")));
     }
     @Test
     public void testDisplayAddAutor() {
         onView(withId(R.id.btnEscultura)).perform(click());
         onView(withId(R.id.btnAddAutor)).perform(click());
-        onView(allOf(withId(R.id.toolbar),withText("Nuevo Autor")));
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Nuevo Autor")));
 
     }
     @Test
@@ -116,7 +130,8 @@ public class BasicTest {
                 .atPosition(0)
                 .perform(click());
         onView(withId(R.id.btnAddObra)).perform(click());
-        onView(allOf(withId(R.id.toolbar),withText("Nueva Obra")));
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Nueva Obra")));
 
     }
 
